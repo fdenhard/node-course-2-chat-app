3c17fde776bcb99837c9f27b674502cb58d00f9a
Finn Denhard <fdenhard@Finns-MacBook-Pro.local>
2017-08-01 10:15:26 -0400

    add and create messages

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'john',
+    text: 'ok cya'
+  });
+
+  socket.on('createMessage', function(message) {
+    console.log('createMessage', message)
+  })
   socket.on('disconnect', () => {
     console.log('Disconnected to server');
   })
