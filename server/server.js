e0dd1542c78843118ad803b06718de8a6bfeeccb
Finn Denhard <fdenhard@Finns-MacBook-Pro.local>
2017-08-02 09:56:53 -0400

    added form and message history

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 var app = express();
@@ -14,31 +15,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'welcome boyo',
-    createdAt: new Date().getTime()
-  })
+  socket.emit('newMessage', generateMessage('admin', 'welcome boyo'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'a boyo has joined',
-    createdAt: new Date().getTime()
-  })
+  socket.broadcast.emit('newMessage', generateMessage('admin', 'a boyo has joined'))
 
-  socket.on('createMessage', function(message) {
+  socket.on('createMessage', (message, cb) => {
     console.log('createMessage', message)
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    })
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    cb('this is from the server');
     // socket.broadcast.emit('createMessage', {
     //   from: message.from,
     //   text: message.text,
     //   createdAt: new Date().getTime()
     // })
-  })
+  });
+
   socket.on('disconnect', () => {
     console.log('Disconnected to server');
   })
