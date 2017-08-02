a23acbfbd001bcc0c12542733eda263cf111c051
Finn Denhard <fdenhard@Finns-MacBook-Pro.local>
2017-08-02 14:44:11 -0400

    added css

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage , generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000
 var app = express();
@@ -17,19 +17,17 @@ io.on('connection', (socket) => {
 
   socket.emit('newMessage', generateMessage('admin', 'welcome boyo'));
 
-  socket.broadcast.emit('newMessage', generateMessage('admin', 'a boyo has joined'))
+  socket.broadcast.emit('newMessage', generateMessage('admin', 'a boyo has joined'));
 
   socket.on('createMessage', (message, cb) => {
     console.log('createMessage', message)
     io.emit('newMessage', generateMessage(message.from, message.text));
-    cb('this is from the server');
-    // socket.broadcast.emit('createMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
+    cb();
   });
 
+  socket.on('createLocationMessage', coords => {
+    io.emit('newLocationMessage', generateLocationMessage('admin', `${coords.latitude}, ${coords.longitude}`));
+  })
   socket.on('disconnect', () => {
     console.log('Disconnected to server');
   })
